31167ea39ad46f4278abaf6c75e9456cd2c09d05
fluffy <klim44@inbox.ru>
2026-04-06 20:03:43 +0500

    Описал основную архитектуру по FSD, сделал небольшую часть глобального стейта

---
+++ src/pages/home/ui/HomePage.tsx
@@ -0,0 +1,12 @@
+export const HomePage = () => {
+    return (
+        <div className="
+            bg-yellow-200 border-2 border-black rounded-xl w-1/6 h-24 alig mx-auto
+            flex justify-center items-center
+        ">
+            <span className="text-[#272727]">
+                Здесь ведутся технические работы
+            </span>
+        </div>
+    );
+};
